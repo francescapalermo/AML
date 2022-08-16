dbd2dd75178cebeac469ff06576e7ca3fccc7c5d
Alex Capstick <56758281+alexcapstick@users.noreply.github.com>
2022-08-16 15:45:16 +0100

    Adding IQR and Documentation

--- aml/utils/__init__.py
+++ aml/utils/__init__.py
@@ -1,5 +1,5 @@
 from .argument_parsing import ArgFake
-from .misc_functions import countna, format_mean_iqr_missing
+from .misc_functions import countna, format_mean_iqr_missing, interquartile_range
 from .progress import tqdm_style
 
 
@@ -7,5 +7,6 @@ __all__ = [
     'ArgFake',
     'countna',
     'format_mean_iqr_missing',
+    'interquartile_range',
     'tqdm_style',
 ]
--- aml/utils/misc_functions.py
+++ aml/utils/misc_functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def countna(array:np.ndarray, normalise:bool=True,)->float:
+def countna(array:np.ndarray, normalise:bool=True,) -> float:
     '''
     Function to calculate the number of NAs in
     an array.
@@ -15,12 +15,14 @@ def countna(array:np.ndarray, normalise:bool=True,)->float:
     
     - `normalise`: `bool`, optional:
         Whether to return the values as a percentage.
+        Defaults to `True`.
     
     Returns
     ---------
 
     - `countna`: `float`:
-        A `float` containing the result.
+        A `float` equal to the number or proportion
+        of missing values in an array.
 
     '''
     count_na = np.count_nonzero(np.isnan(array))
@@ -29,9 +31,76 @@ def countna(array:np.ndarray, normalise:bool=True,)->float:
     return count_na
 
 
+def interquartile_range(values:np.ndarray, lower:float=25, upper:float=75) -> float:
+    '''
+    Function to calculate the interquartile
+    range of an array.
 
-def format_mean_iqr_missing(values, string="{mean:.2f} ({iqr:.2f}) (({count_na:.0f}%))"):
+    Arguments
+    ---------
+
+    - `array`: `numpy.ndarray`:
+        The array to calculate the IQR of.
+    
+    - `lower`: `float`, optional:
+        The percentile of the lower quartile.
+        Defaults to `25`.
+    
+    - `upper`: `float`, optional:
+        The percentile of the upper quartile.
+        Defaults to `75`.
+
+    Returns
+    ---------
+
+    - `iqr`: `float`:
+        A `float` equal to the interquartile range.
+
+    '''
+    return np.subtract(*np.nanpercentile(values, [upper, lower]))
+
+
+def format_mean_iqr_missing(
+    values:np.ndarray, 
+    string:str="{mean:.2f} ({iqr:.2f}) (({count_na:.0f}%))",
+    ) -> str:
+    '''
+    A function useful for formatting a table with information 
+    on the mean, IQR and missing rate of an attribute.
+
+    Examples
+    ---------
+    ```
+    >>> import numpy as np
+    >>> format_mean_iqr_missing(
+            values=np.array([1,2,3,4,5]),
+            string="{mean:.2f} ({iqr:.2f}) (({count_na:.0f}%))",
+            )
+    '3.00 (2) ((0%))'
+    ```
+
+    Arguments
+    ---------
+
+    - `values`: `numpy.ndarray`:
+        The array to calculate the values on.
+    
+    - `string`: `str`, optional:
+        The string that dictates the output.
+        This should include somewhere `{mean}`,
+        `{iqr}`, and `{count_na}`.
+        Defaults to `"{mean:.2f} ({iqr:.2f}) (({count_na:.0f}%))"`.
+    
+    Returns
+    ---------
+
+    - `stats`: `str`:
+        A string of the desired format with the 
+        statistics included.
+
+    
+    '''
     mean = np.mean(values)
-    iqr = np.subtract(*np.nanpercentile(values, [75, 25]))
+    iqr = interquartile_range(values)
     count_na = countna(values)*100
     return string.format(mean=mean, iqr=iqr, count_na=count_na)
