3c63479435a8543958fb333f834fab7b69a79105
Alex Capstick <alexcapstick97@gmail.com>
2022-09-24 14:07:59 +0100

    Rearranging Functions

--- aml/metric/__init__.py
+++ aml/metric/__init__.py
@@ -1,5 +1,16 @@
 from .confusion_matrix import make_confusion_matrix
+from .metric_functions import (
+    countna, 
+    format_mean_iqr_missing, 
+    format_mean_std,
+    interquartile_range,
+    )
+
 
 __all__ = [
     'make_confusion_matrix',
+    'countna',
+    'format_mean_iqr_missing',
+    'format_mean_std',
+    'interquartile_range',
 ]
--- aml/utils/__init__.py
+++ aml/utils/__init__.py
@@ -1,9 +1,6 @@
 from .argument_parsing import ArgFake
+from .directory import dirtree
 from .misc_functions import (
-    countna, 
-    format_mean_iqr_missing, 
-    format_mean_std,
-    interquartile_range,
     module_from_file,
     time_interval,
     )
@@ -12,10 +9,7 @@ from .progress import tqdm_style
 
 __all__ = [
     'ArgFake',
-    'countna',
-    'format_mean_iqr_missing',
-    'format_mean_std',
-    'interquartile_range',
+    'dirtree',
     'module_from_file',
     'time_interval',
     'ProgressParallel',
