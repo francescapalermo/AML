c88f649e70de325629860682bec5322fa2e3b156
Alex Capstick <alexcapstick97@gmail.com>
2022-12-20 11:18:55 +0000

    JSON Encoder

--- aml/utils/__init__.py
+++ aml/utils/__init__.py
@@ -1,9 +1,11 @@
 from .argument_parsing import ArgFake
 from .directory import dirtree
 from .importing import module_from_file
+from .json_encoding import NPEncoder
 
 __all__ = [
     'ArgFake',
     'dirtree',
     'module_from_file',
+    'NPEncoder'
 ]
